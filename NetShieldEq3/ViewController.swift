f1e65ab0e5a7fd27ee6488bff4b01392f90763ed
Mariana <marig-777>
2026-09-11 18:41:13 -0600

    logica para validar usuario y contrasena correctos

--- NetShieldEq3/ViewController.swift
+++ NetShieldEq3/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var email: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        email.keyboardType = .emailAddress
+        email.textContentType = .emailAddress
+        
+        password.textContentType = .password
+        password.isSecureTextEntry = true
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func IniciarSesion(_ sender: UIButton) {
+         let correo = email.text ?? ""
+         let contrasena = password.text ?? ""
+        
+        if correo == "usuario@gmail.com" && contrasena == "123456"{
+            performSegue(withIdentifier: "irAInicio", sender: self)
+        }else{
+            print("correo o password incorrectos")
+        }
+    }
+    
 
 }
 
